d87c42e7481b3805a5510c53866fbaa63f9b562a
Melissa <melissa.pacheco.martinez@gmail.com>
2017-08-11 11:56:04 -0400

    agregando modal que no funciona

--- dist/js/script.js
+++ dist/js/script.js
@@ -20254,7 +20254,6 @@ if (jQuery) {
 })(jQuery);
 
 
-
 $(document).ready(function() {
 
 	$(".m_select").change(function(){
@@ -20279,15 +20278,18 @@ $(".m_btn").click(function(e) {
 	})
 	.done(function(er) {
 		console.log("success");
+		
 		er.forEach(function(e){
-			$(".m_show").append('<div class="m_movie row"><div class="col s6"><h2 class="m_title">'+e.show_title+'</h2>'+
+			var el_id = e.show_id;
+			$(".m_show").append('<div class="m_movie row"><div class="col s6"><a class="m_a modal-trigger" href="#'+el_id+'"><h2 class="m_title">'+e.show_title+'</h2></a>'+
 								'<h4 class="m_year"> Year: '+e.release_year+'</h4>'+
 								'<h4 class="m_category"> Category: '+e.category+'</h4>'+
 								'<h4 class="m_time"> Time: '+e.runtime+'</h4></div>'+
 								'<div class="col s6"><img class="responsive-img m_img" src="'+e.poster+'"></div></div>')							
-								
+							
 		})
-		
+
+			
 	})
 	.fail(function() {
 		console.log("error");
@@ -20297,5 +20299,29 @@ $(".m_btn").click(function(e) {
 	});
 		
 });
-
 });
+
+function modal(er){
+	er.forEach(function(v){
+	var el_id = v.show_id;
+				$(".m_a").click(function(cv){
+					var modal = ('<div id="'+el_id+'" class="modal">'+
+								    '<div class="modal-content">'+
+								      '<h4>'+cv.show_title+'</h4>'+
+								      '<p>'+cv.description+'</p>'+
+								    '</div>'+
+								    '<div class="modal-footer">'+
+								      '<a href="#!" class="modal-action modal-close btn-flat">Close</a>'+
+								    '</div>'+
+								 '</div>');
+
+					$('.modal').show();
+		 			$('body').append(modal);
+		  			$('.modal-close').on('click', function(e) {
+		  				$('.modal').remove();
+
+					});
+				})
+
+		})
+}
